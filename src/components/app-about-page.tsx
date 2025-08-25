d12a7787f8001b1989d42a3a807f620eacc588f2
andreasalberto11 <andreasalberto11@gmail.com>
2025-08-25 10:24:54 +0700

    add dark mode theme

--- src/components/app-about-page.tsx
+++ src/components/app-about-page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
 export function AppAboutPage() {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
+  return (
+    <div className="h-screen pt-30" id="about">
+      <p className="font-bold text-6xl">About Me</p>
+      <Image
+        src={"/hero.png"}
+        width={100}
+        height={100}
+        alt="hero"
+        className=""
+      />
+      <p></p>
+    </div>
+  );
 }
